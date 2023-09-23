5ebca39917d72e7e31455fe64d33c8ca4fde8c94
Ajmain Nishu <ajmainnishu@gmail.com>
2023-09-23 15:52:24 +0600

    feat: chocolate put added

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5ffrq.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const chocolatesCollection = client.db('chocolateDB').collection('chocolate');
+        app.post('/chocolates', async (req, res) => {
+            const data = req.body;
+            console.log(data);
+            const result = await chocolatesCollection.insertOne(data);
+            res.send(result);
+        })
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
-  res.send('Chocolate Management System Server Running')
+    res.send('Chocolate Management System Server Running')
 })
 
 app.listen(port);
